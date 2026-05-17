44e5d48d7711f46a3a90e001d989e12d14e121f5
SimbiOS/Matverse <luamatverse@gmail.com>
2026-05-16 21:54:53 -0300

    Add package initializer

---
+++ src/matverse_skill_governance/__init__.py
@@ -0,0 +1,8 @@
+from .engine import Decision, GovernanceEngine, GovernanceError, evaluate_skill_request
+
+__all__ = [
+    "Decision",
+    "GovernanceEngine",
+    "GovernanceError",
+    "evaluate_skill_request",
+]
